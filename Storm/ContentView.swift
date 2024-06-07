2f3651895b93f226fd270ca56fc12edb243bfa7a
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-08 00:45:07 +0300

    Initial Commit

---
+++ Storm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 8.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Storm/StormApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormApp.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 8.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct StormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
