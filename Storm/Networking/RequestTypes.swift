6d42259f7dd883af3252f797b9abb4d408a88b0c
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-19 14:08:10 +0300

    feat: RequestType added

---
+++ Storm/Networking/RequestTypes.swift
@@ -0,0 +1,15 @@
+//
+//  RequestTypes.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 19.06.2024.
+//
+
+import Foundation
+
+enum RequestType: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
