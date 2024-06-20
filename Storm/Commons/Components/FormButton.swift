7c6aa44ad1aa2828d5d771fd5b6194deb16929fa
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-21 00:58:47 +0300

    feat: ProgressView for loading added

--- Storm/Commons/Components/FormButton.swift
+++ Storm/Commons/Components/FormButton.swift
@@ -10,19 +10,30 @@ import SwiftUI
 struct FormButton: View {
     let title: LocalizedStringKey
     let onPress: () -> Void
+    let isLoading: Bool
     var light: Bool
     
-    init(_ title: LocalizedStringKey, onPress: @escaping () -> Void, light: Bool = false) {
+    init(_ title: LocalizedStringKey, onPress: @escaping () -> Void, isLoading: Bool = false,  light: Bool = false) {
         self.title = title
         self.onPress = onPress
+        self.isLoading = isLoading
         self.light = light
     }
     
     var body: some View {
-        Button(action: onPress, label: {
-            Text(title)
-                .padding(8)
-                .frame(width: 240)
+        Button(action: !isLoading ? onPress: {}, label: {
+            if !isLoading {
+                Text(title)
+                    .padding(8)
+                    .frame(width: 240)
+            } 
+            else {
+                ProgressView()
+                    .imageScale(.large)
+                    .padding(8)
+                    .frame(width: 240)
+                    .tint(.main)
+            }
         })
         .buttonStyle(BorderedProminentButtonStyle())
         .clipShape(RoundedRectangle(cornerRadius: .greatestFiniteMagnitude))
