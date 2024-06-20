920c48bebba800d66d66f2a3393e22c18622fa16
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-20 12:50:26 +0300

    feat: register function

--- Storm/ViewModels/AuthenticationViewModel.swift
+++ Storm/ViewModels/AuthenticationViewModel.swift
@@ -6,13 +6,40 @@
 //
 
 import Foundation
+import Combine
+import SwiftUI
 
 @Observable class AuthenticationViewModel {
     private let auth = FirebaseManager.shared.auth
-    var userInfo = RegisterInfo()
+    var registerInfo = RegisterInfo()
     var isChecked: Bool = false
+    var errorMessage: LocalizedStringKey = ""
+    var successMessage: LocalizedStringKey = ""
+    var showError: Bool = false
+    var showSuccess: Bool = false
+    
+    private var cancellable: AnyCancellable?
     
     func register() {
-        
+        if !isChecked {
+            setError("Please agree with terms")
+            return
+        }
+        cancellable = UserManager.shared.register(info: registerInfo)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    self.showSuccess.toggle()
+                case .failure(let error):
+                    self.setError("Error: \(error.localizedDescription)")
+                }
+            }, receiveValue: { (message: SuccessMessage) in
+                self.successMessage = LocalizedStringKey(message.message)
+            })
+    }
+    
+    private func setError(_ error: String) {
+        self.errorMessage = LocalizedStringKey(error)
+        self.showError = true
     }
 }
