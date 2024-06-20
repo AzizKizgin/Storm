9676fbda909e72c47d24f312f32644e8594709d0
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-20 12:50:00 +0300

    feat: SuccessMessage struct added

---
+++ Storm/Models/SuccessMessage.swift
@@ -0,0 +1,13 @@
+//
+//  SuccessMessage.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 20.06.2024.
+//
+
+import Foundation
+
+struct SuccessMessage: Decodable {
+    let message: String
+    let createdAt: String
+}
