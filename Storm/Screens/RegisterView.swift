8581454a11497f7d4d269481f8f181e0ea930bab
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-21 16:14:14 +0300

    refactor: authenticationVM changed to registerVM

--- Storm/Screens/RegisterView.swift
+++ Storm/Screens/RegisterView.swift
@@ -9,24 +9,25 @@ import SwiftUI
 
 @MainActor
 struct RegisterView: View {
-    @Bindable private var authenticationVM = RegisterViewModel()
+    @Environment(\.dismiss) private var dismiss
+    @Bindable private var registerVM = RegisterViewModel()
     var body: some View {
         VStack {
             TopMessage(title: "Register", message: "Sign up and start connecting!")
             Spacer()
             VStack(spacing: 75) {
                 VStack(spacing: 32){
-                    FormInput("username", text: $authenticationVM.registerInfo.username)
-                    FormInput("email", text: $authenticationVM.registerInfo.email, type: .email)
-                    FormInput("password", text: $authenticationVM.registerInfo.password, type: .password)
-                    FormInput("confirm password", text: $authenticationVM.registerInfo.confirmPassword, type: .password)
-                    CheckBox(isChecked: $authenticationVM.isChecked, title: "I agree with terms and conditions")
+                    FormInput("username", text: $registerVM.registerInfo.username)
+                    FormInput("email", text: $registerVM.registerInfo.email, type: .email)
+                    FormInput("password", text: $registerVM.registerInfo.password, type: .password)
+                    FormInput("confirm password", text: $registerVM.registerInfo.confirmPassword, type: .password)
+                    CheckBox(isChecked: $registerVM.isChecked, title: "I agree with terms and conditions")
                         .underline()
                 }
                 .padding(.horizontal)
-                FormButton("Sign up", onPress: authenticationVM.register, isLoading: authenticationVM.isLoading)
+                FormButton("Sign up", onPress: registerVM.register, isLoading: registerVM.isLoading)
             }
-            .alert(authenticationVM.errorMessage, isPresented: $authenticationVM.showError){
+            .alert(registerVM.errorMessage, isPresented: $registerVM.showError){
                 Button("Okay", role: .cancel) {}
             }
             Spacer()
@@ -34,6 +35,11 @@ struct RegisterView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
         .background(.main)
+        .onChange(of: registerVM.isSuccess) { _ , _ in
+            if registerVM.isSuccess {
+                dismiss()
+            }
+        }
     }
 }
 
