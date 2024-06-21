608a66dd605b2dfa9353ad74a70d90a7f4246206
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-21 16:13:10 +0300

    feat: LoginInfo struct added

---
+++ Storm/Models/LoginInfo.swift
@@ -0,0 +1,13 @@
+//
+//  LoginInfo.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 21.06.2024.
+//
+
+import Foundation
+
+struct LoginInfo: Encodable {
+    var email: String = ""
+    var password: String = ""
+}
