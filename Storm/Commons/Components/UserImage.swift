81089da1c523f9f648c82ca576721f9b7f21b4fd
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-24 23:02:21 +0300

    feat: UserImage updated

--- Storm/Commons/Components/UserImage.swift
+++ Storm/Commons/Components/UserImage.swift
@@ -9,29 +9,38 @@ import SwiftUI
 
 struct UserImage: View {
     var userImage: String = ""
-    var size: CGFloat = 175
+    var size: CGFloat = 150
+    
     var body: some View {
-        Circle()
-            .stroke(.accent, lineWidth: 2)
-            .frame(width: size)
-            .background{
-                ZStack {
-                    Circle().fill(.main)
-                    if !userImage.isEmpty, let image = ImageManager.shared.convertStringToImage(for: userImage){
-                        Image(uiImage: image)
-                            .resizable()
-                            .scaledToFill()
-                            .clipShape(Circle())
-                    }
-                    else {
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: size / 1.6)
-                            .foregroundStyle(.accent)
-                    }
-                }
+        ZStack {
+            Circle()
+                .stroke(.accent, lineWidth: 2)
+                .fill(.main)
+                .frame(width: size)
+            
+            if let uiImage = loadImage() {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size, height: size)
+                    .clipShape(Circle())
+            } else {
+                defaultImage
             }
+        }
+    }
+    
+    private func loadImage() -> UIImage? {
+        guard !userImage.isEmpty else { return nil }
+        return ImageManager.shared.convertStringToImage(for: userImage)
+    }
+    
+    private var defaultImage: some View {
+        Image(systemName: "person.fill")
+            .resizable()
+            .scaledToFit()
+            .frame(width: size / 1.6)
+            .foregroundStyle(.accent)
     }
 }
 
