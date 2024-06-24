46813a84007561c1adab59bf015bb11776105be8
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-24 23:25:47 +0300

    refactor: firebase removed

--- Storm/Firebase/FirebaseManager.swift
+++ Storm/Firebase/FirebaseManager.swift
@@ -5,20 +5,20 @@
 //  Created by Aziz Kızgın on 9.06.2024.
 //
 
-import Foundation
-import FirebaseFirestore
-import FirebaseAuth
-import FirebaseCore
-
-class FirebaseManager: NSObject {
-    let firestore: Firestore
-    let auth: Auth
-    static let shared = FirebaseManager()
-    
-    override init() {
-        self.firestore = Firestore.firestore()
-        self.auth = Auth.auth()
-        
-        super.init()
-    }
-}
+//import Foundation
+//import FirebaseFirestore
+//import FirebaseAuth
+//import FirebaseCore
+//
+//class FirebaseManager: NSObject {
+//    let firestore: Firestore
+//    let auth: Auth
+//    static let shared = FirebaseManager()
+//    
+//    override init() {
+//        self.firestore = Firestore.firestore()
+//        self.auth = Auth.auth()
+//        
+//        super.init()
+//    }
+//}
--- Storm/StormApp.swift
+++ Storm/StormApp.swift
@@ -6,21 +6,21 @@
 //
 
 import SwiftUI
-import FirebaseCore
+//import FirebaseCore
 import SwiftData
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
 
 @main
 struct StormApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var modelContainer: ModelContainer = {
         let schema = Schema([
             User.self
