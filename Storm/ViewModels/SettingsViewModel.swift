8a5835063c937674c033fc73accb1f66798949c1
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-27 23:42:59 +0300

    feat: SettingsViewModel functions added

--- Storm/ViewModels/SettingsViewModel.swift
+++ Storm/ViewModels/SettingsViewModel.swift
@@ -6,9 +6,92 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
 
-@MainActor
 @Observable
 class SettingsViewModel {
+    var usernameInfo = UpdateUsernameData()
+    var aboutInfo = UpdateUserAboutData()
+    var imageInfo = UpdateUserImageData()
+    var errorMessage: LocalizedStringKey = ""
+    var showError: Bool = false
+    var isLoading: Bool = false
+    var isSuccess: Bool = false
     
+    private var cancellable: AnyCancellable?
+    
+    deinit {
+        cancellable?.cancel()
+     }
+    
+    func changeUsername() {
+        if usernameInfo.username.isEmpty {
+            setError("Username must have at least 1 character")
+            return
+        }
+        self.isLoading = true
+        cancellable = UserDataManager.shared.changeUsername(data: usernameInfo)
+            .sink(receiveCompletion: { result in
+                self.isLoading = false
+                switch result {
+                case .finished:
+                    print("finished")
+                case .failure(let error):
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { _ in
+                Task {
+                    await UserDataSource.shared.updateProperty(prop: \.username, newValue: self.usernameInfo.username)
+                    self.isSuccess = true
+                    self.isLoading = false
+                }
+            })
+    }
+    
+    func changeAbout() {
+        self.isLoading = true
+        cancellable = UserDataManager.shared.changeUserAbout(data: aboutInfo)
+            .sink(receiveCompletion: { result in
+                self.isLoading = false
+                switch result {
+                case .finished:
+                    print("finished")
+                case .failure(let error):
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { _ in
+                Task {
+                    await UserDataSource.shared.updateProperty(prop: \.about, newValue: self.aboutInfo.about)
+                    self.isSuccess = true
+                    self.isLoading = false
+                }
+            })
+    }
+    
+    func changeImage() {
+        self.isLoading = true
+        cancellable = UserDataManager.shared.changeUserImage(data: imageInfo)
+            .sink(receiveCompletion: { result in
+                self.isLoading = false
+                switch result {
+                case .finished:
+                    print("finished")
+                case .failure(let error):
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { _ in
+                Task {
+                    await UserDataSource.shared.updateProperty(prop: \.profilePicture, newValue: self.imageInfo.profilePicture)
+                    self.isSuccess = true
+                    self.isLoading = false
+                }
+            })
+    }
+    
+    
+    private func setError(_ error: String) {
+        self.errorMessage = LocalizedStringKey(error)
+        self.showError = true
+    }
 }
