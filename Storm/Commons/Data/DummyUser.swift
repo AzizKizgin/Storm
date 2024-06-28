144e337e1de77e3db7791ee41dbfd84b7f15e1a3
Aziz Kızgın <azizkzgn@gmail.com>
2024-06-28 12:44:56 +0300

    feat: dummy user added

---
+++ Storm/Commons/Data/DummyUser.swift
@@ -0,0 +1,10 @@
+//
+//  DummyUser.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 28.06.2024.
+//
+
+import Foundation
+
+let dummyUser = User(id: "userId", email: "user@dummy.com", username: "dummy user", about: "dummy about", createdAt: "12.04.2024", profilePicture: userImagePlaceholder3)
