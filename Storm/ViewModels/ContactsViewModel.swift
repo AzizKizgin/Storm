9e0c049e4ebb058a81461d228e298f7aecc28188
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-01 22:32:59 +0300

    feat: ContactsViewModel created

---
+++ Storm/ViewModels/ContactsViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  ContactsViewModel.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 1.07.2024.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+@Observable class ContactsViewModel {
+    var searchResult: SearchUsersResponse?
+    var queryObject: SearchUsersQuery = SearchUsersQuery()
+    var isChecked: Bool = false
+    var errorMessage: LocalizedStringKey = ""
+    var showError: Bool = false
+    var isLoading: Bool = false
+    var isSuccess: Bool = false
+    
+    @ObservationIgnored
+    private var cancellable: AnyCancellable?
+    
+    deinit {
+        cancellable?.cancel()
+     }
+    
+    func searchUsers() {
+//        if !self.queryObject.username.isEmpty {
+//            return
+//        }
+        print("sss222s")
+        self.isLoading = true
+        cancellable = ContactDataManager.shared.searchUsers(query: self.queryObject)
+            .sink(receiveCompletion: { completionResult in
+                self.isLoading = false
+                switch completionResult {
+                case .finished:
+                    print("finished")
+                    print(self.searchResult?.users ?? "")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { result in
+                Task {
+                    self.searchResult = result
+                    self.isSuccess = true
+                    self.isLoading = false
+                }
+            })
+    }
+    
+    private func setError(_ error: String) {
+        self.errorMessage = LocalizedStringKey(error)
+        self.showError = true
+    }
+    
+}
