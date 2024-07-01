39d6e447e7557578ff9a90b8e5a5e09107ba6e21
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-01 22:32:11 +0300

    feat: SearchUsersQuery added

---
+++ Storm/Helpers/SearchUsersQuery.swift
@@ -0,0 +1,28 @@
+//
+//  SearchUsersQuery.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 1.07.2024.
+//
+
+import Foundation
+
+struct SearchUsersQuery: Encodable, Equatable {
+    var username: String = "U"
+    var page: Int = 1
+    var pageSize: Int = 15
+    
+    func toQueryItems() -> [URLQueryItem] {
+        let mirror = Mirror(reflecting: self)
+        var queryItems = [URLQueryItem]()
+        
+        for case let (label?, value) in mirror.children {
+            if let stringValue = value as? CustomStringConvertible {
+                let stringRepresentation = stringValue.description
+                queryItems.append(URLQueryItem(name: label, value: stringRepresentation))
+            }
+        }
+        return queryItems
+    }
+}
+
