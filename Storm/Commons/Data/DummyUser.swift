6022566e481850111e2d541ff8df99463f8b50c3
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-11 22:26:07 +0300

    feat: dummydatas added

--- Storm/Commons/Data/DummyUser.swift
+++ Storm/Commons/Data/DummyUser.swift
@@ -8,3 +8,13 @@
 import Foundation
 
 let dummyUser = User(id: "userId", email: "user@dummy.com", username: "dummy user", about: "dummy about", createdAt: "12.04.2024", profilePicture: userImagePlaceholder3)
+
+let dummyUser2 = User(id: "userId2", email: "user2@dummy.com", username: "dummy user 2", about: "dummy about 2", createdAt: "12.04.2024", profilePicture: userImagePlaceholder2)
+
+let dummyUser3 = User(id: "userId3", email: "user3@dummy.com", username: "dummy user 3", about: "dummy about 3", createdAt: "12.04.2024", profilePicture: userImagePlaceholder)
+
+let dummyUserResponse = UserResponse(id: "userId", email: "user@dummy.com", username: "dummy user", about: "dummy about", createdAt: "12.04.2024", profilePicture: userImagePlaceholder3)
+
+let dummyUserResponse2 = UserResponse(id: "userId2", email: "user2@dummy.com", username: "dummy user 2", about: "dummy about 2", createdAt: "12.04.2024", profilePicture: userImagePlaceholder2)
+
+let dummyUserResponse3 = UserResponse(id: "userId3", email: "user3@dummy.com", username: "dummy user 3", about: "dummy about 3", createdAt: "12.04.2024", profilePicture: userImagePlaceholder)
