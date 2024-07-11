8c7599c2cf999564b46697c154df143f8103e6c3
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-11 22:27:34 +0300

    feat: dummy data and list added

--- Storm/Screens/ContactsView.swift
+++ Storm/Screens/ContactsView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ContactsView: View {
+    @Environment(\.undoManager) private var undo
+    @State private var selectedUser: UserResponse?
+    @Namespace var namespace
+    @State private var users: [UserResponse] = [dummyUserResponse, dummyUserResponse2, dummyUserResponse3]
     var body: some View {
-        Text("Contacts")
+        List {
+            ForEach(users, id: \.id){ user in
+                ContactItem(user: user, onItemPress: {}, onImagePress: {selectedUser = user})
+                    .listRowInsets(EdgeInsets(top: 20, leading: 10, bottom: 0, trailing: 10))
+                    .listRowBackground(Color.main)
+                    .listRowSeparator(.hidden)
+                    .swipeActions(allowsFullSwipe: false) {
+                          Button(role: .destructive) {
+                              print("Deleting conversation")
+                          } label: {
+                              Label("Delete", systemImage: "trash.fill")
+                          }
+                      }
+            }
+        }
+        .navigationTitle("Contacts")
+        .listStyle(.inset)
+        .scrollContentBackground(.hidden)
+        .background(.main)
+        .modal(isPresented: .constant(selectedUser != nil), onClose: {
+            selectedUser = nil
+        }) {
+            if let selectedUser {
+                ContactModalItem(contact: selectedUser)
+            }
+        }
     }
 }
 
 #Preview {
-    ContactsView()
+    NavigationStack {
+        ContactsView()
+    }
 }
