a75f4e336532f1970dc57861c0258238da43478d
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-15 22:35:29 +0300

    feat: dummySearchUsersResponse added

--- Storm/Models/SearchUsersResponse.swift
+++ Storm/Models/SearchUsersResponse.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 struct SearchUsersResponse: Decodable {
-    let users: [UserResponse]
+    var users: [UserResponse]
     let page: Int
     let pageSize: Int
     let totalPages: Int
 }
+
+let dummySearchUsersResponse = SearchUsersResponse(users: [dummyUserResponse, dummyUserResponse2, dummyUserResponse3], page: 1, pageSize: 1, totalPages: 1)
