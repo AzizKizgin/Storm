9928a2aebd8b922ef23276f57011a6ca8f0c0a5f
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-17 01:22:10 +0300

    feat: updated

--- Storm/Commons/Components/ContactItem.swift
+++ Storm/Commons/Components/ContactItem.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct ContactItem: View {
+    @State private var showModal: Bool = false
     let user: UserResponse
-    let onItemPress: () -> Void
-    let onImagePress: () -> Void
-    var onAddPress: () -> Void = {}
+    var onItemPress: () -> Void = {}
+    var onAddContact: () -> Void = {}
+    var onRemoveContact: () -> Void = {}
     var isCurrentUser: Bool = false
+    var isContact: Bool = false
+    
+    init(user: UserResponse) {
+        self.user = user
+    }
     var body: some View {
         HStack(spacing: 0){
             UserImage(userImage: user.profilePicture ?? "", size: 50)
                 .padding(.trailing)
                 .onTapGesture {
-                    onImagePress()
+                    UIView.setAnimationsEnabled(false)
+                    showModal.toggle()
                 }
             VStack {
                 Text(isCurrentUser ? "You" : user.username)
@@ -30,20 +37,85 @@ struct ContactItem: View {
                     .foregroundStyle(.secondary)
             }
             .lineLimit(1)
-            if !isCurrentUser {
-                Button(action: {}, label: {
-                    Image(systemName: "person.fill.badge.plus")
-                        .foregroundStyle(.accent)
-                })
-                .buttonStyle(PlainButtonStyle())
+            Group {
+                if !isCurrentUser {
+                    if isContact {
+                        Button(action: {}, label: {
+                            Image(systemName: "person.fill.badge.minus")
+                                .foregroundStyle(.accent)
+                        })
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                    else {
+                        Button(action: {}, label: {
+                            Image(systemName: "person.fill.badge.plus")
+                                .foregroundStyle(.accent)
+                        })
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
             }
-
+            .font(.title2)
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+            onItemPress()
         }
         .padding(.horizontal)
         .background(.main)
+        .fullScreenCover(isPresented: $showModal) {
+            ZStack {
+                Color.accent
+                    .opacity(0.1)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        showModal.toggle()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                            UIView.setAnimationsEnabled(true)
+                        }
+                    }
+                ContactModalItem(contact: user)
+            }
+            .presentationBackground(.clear)
+        }
     }
 }
 
+extension ContactItem {
+    func onPress(_ onPress: @escaping () -> Void) -> ContactItem {
+        var copy = self
+        copy.onItemPress = onPress
+        return copy
+    }
+    
+    func onAddContact(_ onAddContact: @escaping () -> Void) -> ContactItem {
+        var copy = self
+        copy.onAddContact = onAddContact
+        return copy
+    }
+    
+    func onRemoveContact(_ onRemoveContact: @escaping () -> Void) -> ContactItem {
+        var copy = self
+        copy.onRemoveContact = onRemoveContact
+        return copy
+    }
+    
+    func isCurrentUser(_ isCurrentUser: Bool) -> ContactItem {
+        var copy = self
+        copy.isCurrentUser = isCurrentUser
+        return copy
+    }
+    
+    func isContact(_ isContact: Bool) -> ContactItem {
+        var copy = self
+        copy.isContact = isContact
+        return copy
+    }
+    
+}
+
 #Preview {
-    ContactItem(user: dummyUserResponse, onItemPress: {}, onImagePress: {}, isCurrentUser: true)
+    ContactItem(user: dummyUserResponse)
+        .onPress {}
+        .isContact(true)
 }
