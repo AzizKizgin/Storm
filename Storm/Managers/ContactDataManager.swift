e62d0a5140dedec315aede68e5d0bfd022518195
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-19 21:41:21 +0300

    feat: contact operation functions added

--- Storm/Managers/ContactDataManager.swift
+++ Storm/Managers/ContactDataManager.swift
@@ -18,4 +18,33 @@ class ContactDataManager {
             requestType: .get
         )
     }
+    
+    func addContact(createContactInfo: CreateContactDto) -> AnyPublisher<SuccessMessage, Error> {
+        return DataManager.shared.sendRequest(
+            for: Endpoints.addContact(),
+            data: createContactInfo,
+            requestType: .post
+        )
+    }
+    
+    func removeContact(contactId: String) -> AnyPublisher<SuccessMessage, Error> {
+        return DataManager.shared.sendRequest(
+            for: Endpoints.removeContact(contactUserId: contactId),
+            requestType: .delete
+        )
+    }
+    
+    func removeContact(contactId: Int) -> AnyPublisher<SuccessMessage, Error> {
+        return DataManager.shared.sendRequest(
+            for: Endpoints.removeContact(contactId: contactId),
+            requestType: .delete
+        )
+    }
+    
+    func getAllContacts() -> AnyPublisher<[ContactResponse], Error> {
+        return DataManager.shared.sendRequest(
+            for: Endpoints.getAllContact(),
+            requestType: .get
+        )
+    }
 }
