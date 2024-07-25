5533d32b8ab6cf3eb7e4709b1c55506111eda052
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-25 23:37:16 +0300

    feat: date functions updated

--- Storm/Helpers/Utils.swift
+++ Storm/Helpers/Utils.swift
@@ -63,18 +63,37 @@ struct Utils {
         return ""
     }
     
-    static func getDayDate(_ dateString: String) -> String {
+    static func getChatDate(_ dateString: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ"
         if let date = dateFormatter.date(from: dateString) {
             let formatter = DateFormatter()
-            formatter.setLocalizedDateFormatFromTemplate("dd MMMM")
+            let calendar = Calendar.current
+            if calendar.isDateInToday(date) {
+                formatter.setLocalizedDateFormatFromTemplate("HH:mm")
+            } else if calendar.isDateInThisYear(date) {
+                formatter.setLocalizedDateFormatFromTemplate("dd MMMM")
+            } else {
+                formatter.setLocalizedDateFormatFromTemplate("dd MMMM yyyy")
+            }
+            
             return formatter.string(from: date)
         }
         return ""
     }
     
-    static func formatDate(_ dateString: String) -> String {
+    static func isDatesEqual(_ dateString1: String, _ dateString2: String) -> Bool {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ"
+        if let date1 = dateFormatter.date(from: dateString1), let date2 = dateFormatter.date(from: dateString2){
+            let formatter = DateFormatter()
+            formatter.setLocalizedDateFormatFromTemplate("dd MMMM")
+            return formatter.string(from: date1) == formatter.string(from: date2)
+        }
+        return false
+    }
+    
+    static func getMessageTime(_ dateString: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ"
         
