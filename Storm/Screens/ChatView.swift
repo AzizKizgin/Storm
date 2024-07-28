4c5cc46e50179faf27ccd8da1132f766ecd75d17
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-28 16:51:36 +0300

    feat: chatViewModel used and message operations added

--- Storm/Screens/ChatView.swift
+++ Storm/Screens/ChatView.swift
@@ -6,56 +6,67 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ChatView: View {
+    @Query private var user: [User]
     @Environment(\.dismiss) private var dismiss
-    @State private var text: String = ""
-    @State private var currentMessageDate: String = ""
-    @State private var chat: Chat = dummyChatList[0]
+    @Bindable private var chatVM = ChatViewModel()
+    var chatId: String? = nil
+    var contact: UserResponse? = nil
     var body: some View {
-        let contact = chat.members.first{$0.user.id != "userId"}
         VStack {
             ScrollViewReader { proxy in
-                List(chat.messages,id: \.id) { message in
-                    Group {
-                        VStack {
-                            if message == chat.messages.first {
-                                DateTitle(date: message.createdAt)
+                if !chatVM.messages.isEmpty {
+                    List(chatVM.messages,id: \.id) { message in
+                        HStack {
+                            if message.sender.id == user.first?.id {
+                                Spacer()
                             }
-                            else if let index = chat.messages.firstIndex(of: message),
-                                    Utils.isDatesEqual(chat.messages[index - 1].createdAt,message.createdAt)
-                            {
-                                DateTitle(date: message.createdAt)
-                            }
-                            
+                            MessageItem(message: message,currentUserId: user.first?.id ?? "")
                         }
+                        .listRowInsets(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
                         .listRowBackground( Color.main)
-                        MessageItem(message: message)
-                            .listRowInsets(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                            .listRowBackground( Color.main)
-                            .listRowSeparator(.hidden)
-                            .id(message.id)
+                        .listRowSeparator(.hidden)
+                        .id(message.id)
                     }
+                    .onChange(of: self.chatVM.messages, { oldValue, newValue in
+                        if newValue.count > oldValue.count, let lastItem = chatVM.messages.last?.id {
+                            proxy.scrollTo(lastItem, anchor: .top)
+                        }
+                        chatVM.readChat()
+                    })
+                    .onAppear {
+                        if let readLastItem = chatVM.messages.first(where: {!$0.readBy.contains(user.first?.id ?? "")})?.id {
+                            proxy.scrollTo(readLastItem, anchor: .top)
+                        }
+                        else if let lastItem = chatVM.messages.last?.id {
+                            proxy.scrollTo(lastItem, anchor: .top)
+                        }
+                        chatVM.readChat()
+                    }
+                    .listStyle(.inset)
+                    .scrollContentBackground(.hidden)
                 }
-                .onAppear {
-                    if let lastItem = chat.messages.first(where: {!$0.readBy.contains("userId")})?.id {
-                        proxy.scrollTo(lastItem, anchor: .top)
-                    }
-                    else if let lastItem = chat.messages.last?.id {
-                        proxy.scrollTo(lastItem, anchor: .top)
-                    }
+                else {
+                    VStack {}
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
-                .listStyle(.inset)
-                .scrollContentBackground(.hidden)
             }
-            ChatInput(text: $text) {
-                
+            ChatInput(text: $chatVM.content) {
+                chatVM.sendMessage()
+            }
+        }
+        .onAppear {
+            Task {
+                await chatVM.onAppear(contact: contact, chatId: chatId, appUserId: user.first?.id)
             }
         }
         .background(.main)
         .safeAreaInset(edge: .top) {
-            ChatHeader(user: contact?.user, onPress: {dismiss()})
+            ChatHeader(user: chatVM.contact, onPress: {dismiss()})
         }
+        .navigationBarBackButtonHidden()
     }
 }
 
---
+++ Storm/ViewModels/ChatViewModel.swift
@@ -0,0 +1,280 @@
+//
+//  ChatViewModel.swift
+//  Storm
+//
+//  Created by Aziz Kızgın on 25.07.2024.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import SignalRClient
+
+@Observable class ChatViewModel {
+    var chatId: String?
+    var appUserId: String?
+    var content: String = ""
+    var searchText: String = ""
+    var contact: UserResponse?
+    var messages: [Message] = []
+    var filteredMessages: [Message] = []
+    var selectedChats: [String] = []
+    var errorMessage: LocalizedStringKey = ""
+    var showError: Bool = false
+    var isLoading: Bool = false
+    var showSelectContact: Bool = false
+    
+    @ObservationIgnored
+    private var cancellable: AnyCancellable?
+    @ObservationIgnored
+    private var connection: HubConnection?
+    
+    deinit {
+        cancellable?.cancel()
+        self.leaveRoom()
+    }
+  
+
+    init() {
+        self.startConnection()
+       
+    }
+    
+    func onAppear(contact: UserResponse?, chatId: String?, appUserId: String?) async {
+        self.contact = contact
+        self.chatId = chatId
+        self.appUserId = appUserId
+        await self.getChat()
+        self.joinRoom()
+    }
+
+    private func startConnection() {
+        let url = URL(string: "http://localhost:5117/chatHub")!
+        connection = HubConnectionBuilder(url: url)
+            .build()
+
+        connection?.on(method: "ReceiveMessage", callback: { (message: String) in
+            if let decodedData = Data(base64Encoded: message),
+               let jsonString = String(data: decodedData, encoding: .utf8),
+               let jsonData = jsonString.data(using: .utf8) {
+                do {
+                    let message = try JSONDecoder().decode(Message.self, from: jsonData)
+                    self.handleMessage(result: message)
+                   
+                } catch {
+                    print("Failed to decode JSON: \(error)")
+                }
+            } else {
+                print("Failed to decode Base64 string.")
+            }
+        })
+        
+        connection?.on(method: "RecieveRead", callback: { (userId: String) in
+            self.updateReadBy(for: &self.messages, with: userId)
+            print("a")
+        })
+
+        connection?.start()
+    }
+
+    func sendMessageToHub(message: Message, type: MessageType) {
+        var message = message
+        message.type = type.hashValue
+        let data = try! JSONEncoder().encode(message)
+        connection?.invoke(method: "SendMessage", chatId, data) { error  in
+            if let error = error {
+                print("Error sending message: \(error)")
+            }
+        }
+    }
+    
+    func sendReadInfoToHub() {
+        connection?.invoke(method: "ReadMessage", chatId ,appUserId ){ error in
+            if let error = error {
+                print("Error reading message: \(error)")
+            }
+        }
+    }
+
+    func joinRoom() {
+        connection?.invoke(method: "JoinRoom", chatId) { error in
+            if let error = error {
+                print("Error joining room: \(error)")
+            }
+        }
+    }
+
+    func leaveRoom() {
+        connection?.invoke(method: "LeaveRoom", chatId) { error in
+            if let error = error {
+                print("Error leaving room: \(error)")
+            }
+        }
+    }
+    
+    func getChat() async {
+        guard chatId != nil || contact?.id != nil else {
+            return
+        }
+        self.isLoading = true
+        cancellable = ChatDataManager.shared.getChat(id: (chatId ?? contact?.id)!)
+            .sink(receiveCompletion: { completionResult in
+                self.isLoading = false
+                switch completionResult {
+                case .finished:
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { [self] result in
+                self.contact = result.members.first{$0.user.id != self.appUserId}?.user
+                self.messages = result.messages
+                self.chatId = chatId
+            })
+    }
+    
+    func readChat() {
+        guard let chatId else{
+            setError("Chat not found")
+            return
+        }
+        self.isLoading = true
+        cancellable = ChatDataManager.shared.readChat(id: chatId)
+            .sink(receiveCompletion: { completionResult in
+                self.isLoading = false
+                switch completionResult {
+                case .finished:
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { result in
+                self.sendReadInfoToHub()
+            })
+    }
+    
+    func sendMessage() {
+        guard !content.isEmpty else {
+            return
+        }
+        
+        self.isLoading = true
+        let data = CreateMessageData(
+            content: content,
+            receiverId: contact?.id,
+            chatId: chatId
+        )
+        cancellable = ChatDataManager.shared.sendMessage(data: data)
+            .sink(receiveCompletion: { completionResult in
+                self.isLoading = false
+                switch completionResult {
+                case .finished:
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { result in
+                self.handleMessage(result: result)
+                self.sendMessageToHub(message: result, type: .add)
+            })
+    }
+    
+    func deleteMessage(id: Int) {
+        guard !content.isEmpty else {
+            return
+        }
+        
+        self.isLoading = true
+        cancellable = ChatDataManager.shared.deleteMessage(id: id)
+            .sink(receiveCompletion: { completionResult in
+                self.isLoading = false
+                switch completionResult {
+                case .finished:
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { result in
+                if let index = self.messages.firstIndex(of: result) {
+                    self.messages.remove(at: index)
+                    self.sendMessageToHub(message: result, type: .delete)
+                }
+            })
+    }
+    
+    func editMessage(id: Int) {
+        guard !content.isEmpty else {
+            return
+        }
+        
+        self.isLoading = true
+        
+        cancellable = ChatDataManager.shared.editMessage(id: id, data: EditMessageData(content: content))
+            .sink(receiveCompletion: { completionResult in
+                self.isLoading = false
+                switch completionResult {
+                case .finished:
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.setError(error.localizedDescription)
+                }
+            }, receiveValue: { result in
+                if let index = self.messages.firstIndex(of: result) {
+                    self.messages[index] = result
+                    self.sendMessageToHub(message: result, type: .edit)
+                }
+            })
+    }
+    
+    func search() {
+        if searchText.isEmpty {
+            filteredMessages = messages
+        }
+        else {
+            filteredMessages = searchMessages()
+        }
+    }
+    
+    private func searchMessages() -> [Message] {
+        return messages.filter { message in
+            message.content.contains(searchText)
+        }
+    }
+    
+    private func setError(_ error: String) {
+        self.errorMessage = LocalizedStringKey(error)
+        self.showError = true
+    }
+    
+    private func handleMessage(result: Message) {
+        if let index = self.messages.firstIndex(where: {$0.id == result.id}) {
+            switch result.type {
+            case MessageType.delete.hashValue:
+                self.messages.remove(at: index)
+            case MessageType.edit.hashValue:
+                self.messages[index] = result
+            case MessageType.reaction.hashValue:
+                self.messages[index] = result
+            default:
+                self.messages[index] = result
+            }
+        }
+        else {
+            self.messages.append(result)
+        }
+    }
+    
+    func updateReadBy(for messages: inout [Message], with userId: String) {
+        for i in 0..<messages.count {
+            if !messages[i].readBy.contains(userId) {
+                messages[i].readBy.append(userId)
+            }
+        }
+    }
+}
+
