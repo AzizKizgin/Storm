95f1dc69bab824e17580768f1bd2a3bf59780f14
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-30 21:29:42 +0300

    feat: navigationstack added

--- Storm/Screens/ChatView.swift
+++ Storm/Screens/ChatView.swift
@@ -15,56 +15,58 @@ struct ChatView: View {
     var chatId: String? = nil
     var contact: UserResponse? = nil
     var body: some View {
-        VStack {
-            ScrollViewReader { proxy in
-                if !chatVM.messages.isEmpty {
-                    List(chatVM.messages,id: \.id) { message in
-                        HStack {
-                            if message.sender.id == user.first?.id {
-                                Spacer()
+        NavigationStack {
+            VStack {
+                ScrollViewReader { proxy in
+                    if !chatVM.messages.isEmpty {
+                        List(chatVM.messages,id: \.id) { message in
+                            HStack {
+                                if message.sender.id == user.first?.id {
+                                    Spacer()
+                                }
+                                MessageItem(message: message,currentUserId: user.first?.id ?? "")
                             }
-                            MessageItem(message: message,currentUserId: user.first?.id ?? "")
+                            .listRowInsets(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                            .listRowBackground( Color.main)
+                            .listRowSeparator(.hidden)
+                            .id(message.id)
                         }
-                        .listRowInsets(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                        .listRowBackground( Color.main)
-                        .listRowSeparator(.hidden)
-                        .id(message.id)
+                        .onChange(of: self.chatVM.messages, { oldValue, newValue in
+                            if newValue.count > oldValue.count, let lastItem = chatVM.messages.last?.id {
+                                proxy.scrollTo(lastItem, anchor: .top)
+                            }
+                            chatVM.readChat()
+                        })
+                        .onAppear {
+                            if let readLastItem = chatVM.messages.first(where: {!$0.readBy.contains(user.first?.id ?? "")})?.id {
+                                proxy.scrollTo(readLastItem, anchor: .top)
+                            }
+                            else if let lastItem = chatVM.messages.last?.id {
+                                proxy.scrollTo(lastItem, anchor: .top)
+                            }
+                            chatVM.readChat()
+                        }
+                        .listStyle(.inset)
+                        .scrollContentBackground(.hidden)
                     }
-                    .onChange(of: self.chatVM.messages, { oldValue, newValue in
-                        if newValue.count > oldValue.count, let lastItem = chatVM.messages.last?.id {
-                            proxy.scrollTo(lastItem, anchor: .top)
-                        }
-                        chatVM.readChat()
-                    })
-                    .onAppear {
-                        if let readLastItem = chatVM.messages.first(where: {!$0.readBy.contains(user.first?.id ?? "")})?.id {
-                            proxy.scrollTo(readLastItem, anchor: .top)
-                        }
-                        else if let lastItem = chatVM.messages.last?.id {
-                            proxy.scrollTo(lastItem, anchor: .top)
-                        }
-                        chatVM.readChat()
+                    else {
+                        VStack {}
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
                     }
-                    .listStyle(.inset)
-                    .scrollContentBackground(.hidden)
                 }
-                else {
-                    VStack {}
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                ChatInput(text: $chatVM.content) {
+                    chatVM.sendMessage()
                 }
             }
-            ChatInput(text: $chatVM.content) {
-                chatVM.sendMessage()
+            .onAppear {
+                chatVM.onAppear(contact: contact, chatId: chatId, appUserId: user.first?.id)
             }
+            .background(.main)
+            .safeAreaInset(edge: .top) {
+                ChatHeader(user: chatVM.contact, onPress: {dismiss()})
+            }
+            .navigationBarBackButtonHidden()
         }
-        .onAppear {
-            chatVM.onAppear(contact: contact, chatId: chatId, appUserId: user.first?.id)
-        }
-        .background(.main)
-        .safeAreaInset(edge: .top) {
-            ChatHeader(user: chatVM.contact, onPress: {dismiss()})
-        }
-        .navigationBarBackButtonHidden()
     }
 }
 
